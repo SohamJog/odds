d2ab201497069c12d27ecbba0ab76799854f3ff2
Soham Jog <34083543+SohamJog@users.noreply.github.com>
2022-04-17 17:02:15 +0530

    Update main.sol
    yet to add comments

--- main.sol
+++ main.sol
@@ -4,53 +4,86 @@ pragma solidity ^0.8.13;
 contract MyContract 
 {
 
-    //global variables
-    uint count = 0;         //count of bets()
+    enum State{Default, Confirmed, Claimed, Done}
+    State constant defaultChoice = State.Default;
 
-    struct bet
+
+    struct Bet
     {
         address p1;
-        address p2;          
-        uint amt;           //amount of bet
-        uint betId;         //id of bet
+        address p2;
+        State betState;
+        uint message;
+        uint val;
+        uint tentativewinner;
     }
-    bet[] bets;
-
-    function _createBet() internal 
+    //array of all bets(active and inactive) maybe change this?
+    Bet[] bets;
+    function requestBet(uint _msg, address _to) public payable
     {
-
+        //value = msg.value
+        //person1 = msg.sender
+        bets.push(Bet(msg.sender, _to, State.Default, _msg, uint(msg.value), 0));
     }
 
-    function _deleteBet() internal 
+    function acceptBet(uint _betId) public payable
+    {   
+        Bet storage bet = bets[_betId];
+        if(bet.p2 != msg.sender || msg.value != bet.val || bet.betState!=State.Default)
+        {
+            revert();
+        }
+        bet.betState = State.Confirmed;
+    }
+
+    function rejectBet(uint _betId) public 
+    {   
+        Bet storage bet = bets[_betId];
+        require(bet.p2 == msg.sender && bet.betState==State.Default);
+        
+        bet.betState = State.Done;
+        // pay p1 back
+        //https://ethereum.stackexchange.com/questions/94707/how-to-fix-typeerror-type-address-is-not-implicitly-convertible-to-expected-ty
+        payable(bet.p1).transfer(bet.val);
+    }
+
+    function forfeit(uint _betId) public
     {
+        Bet storage bet = bets[_betId];
+        require((bet.p1==msg.sender || bet.p2==msg.sender)&& bet.betState==State.Confirmed);
+        bet.betState = State.Done;
+        payable(bet.p1).transfer(bet.val);
+        payable(bet.p2).transfer(bet.val);
+    }
+    function claim(uint _betId) public 
+    {
+        Bet storage bet = bets[_betId];
+        require((bet.p1==msg.sender || bet.p2==msg.sender)&& bet.betState==State.Confirmed);
+        bet.betState=State.Claimed;
 
+        if(msg.sender == bet.p1) bet.tentativewinner=1;
+        else bet.tentativewinner = 2;
     }
 
+    function confirm(uint _betId) public
+    {
+        Bet storage bet = bets[_betId];
+        require(((msg.sender==bet.p1&&bet.tentativewinner==2)||(msg.sender==bet.p2&&bet.tentativewinner==1)) && bet.betState==State.Claimed);
+
+        bet.betState = State.Done;
+        address _to;
+        if(bet.tentativewinner==1)_to = bet.p1;
+        else _to = bet.p2;
+
+        payable(_to).transfer(bet.val*2);
+    }
     
-    /*
-    create a map for whether one address approves the existence of betId
-    create a map of what an address returns for a betId (assuming the address is a part of the bet)
-    bet statement string to betId can be done in Python 
-    
-    add onlyOwner to acceptBet and verify
-    */
-
-    function requestBet() public
+    function deny(uint _betId) public
     {
-
+        Bet storage bet = bets[_betId];
+        require(((msg.sender==bet.p1&&bet.tentativewinner==2)||(msg.sender==bet.p2&&bet.tentativewinner==1)) && bet.betState==State.Claimed);
+        bet.betState = State.Confirmed;
     }
-
-    function acceptBet() public 
-    {
-
-    }
-
-    function verify() public
-    {
-
-    }
-
-
 
 
 }
